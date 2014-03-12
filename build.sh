ee31e8c986be823d352ddc99245785a0317566b2
Vilius Lukosius <vilius.lukosius@teliasonera.com>
2014-03-12 23:30:56 +0100

    Removed some intermediate files.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-#
-# Helper script for building docker image.
-
-sudo docker build -rm=true -t=viliusl/ubuntu-sshd-nginx .
--- connect-ssh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-#
-# Helper script for connecting via ssh to running image
-
-ssh root@localhost -p 55522
